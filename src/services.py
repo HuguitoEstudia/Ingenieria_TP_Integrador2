311d04e88fd73722c7d11ac7aaa7b721d9370420
Huguito <110265653+BondiolaAlHorno@users.noreply.github.com>
2025-10-17 21:39:25 -0300

    Update services.cpython-313.pyc and services.py

--- src/services.py
+++ src/services.py
@@ -1,9 +1,8 @@
 from datetime import date
 import json
-from typing import List, Optional
-from fastapi import Depends,Body
 from app import app
 import pymongo
+from bson.objectid import ObjectId
 
 class Response():
     def __init__(self,data):
@@ -26,7 +25,7 @@ MONGO_BASEDATOS="TPintegrador2"
 #-----------------------------------------
 
 @app.post("/create_madurador/",tags=["madurador"])
-def create_lote(litros,estado,notas,lote):
+def create_madurador(litros:int,estado:str,lote:dict,notas:str=""):
     documento = {"litros":litros,
                  "estado":estado,
                  "notas":notas,
@@ -45,6 +44,56 @@ def create_lote(litros,estado,notas,lote):
     cliente.close()
 
 
+@app.post("/update_madurador_by_id/",tags=["madurador"])
+def update_madurador(id,litros=None,estado=None,lote=None,notas=None):
+    
+    objid=ObjectId(id)
+
+    #comprobamos que los atributos no esten vacios antes de modificar para no eliminar ninguno involuntariamente     
+    nuevos_datos = {}
+
+    if litros != None:
+          nuevos_datos["litros"]=int(litros)
+    
+    if estado != None:
+          nuevos_datos["estado"]=int(estado)
+    
+    if lote != None:
+          nuevos_datos["lote"]=int(lote)
+
+    if notas != None:
+          nuevos_datos["notas"]=int(notas)
+    
+    #Defino el string de Conexion
+    cliente=pymongo.MongoClient(MONGO_URI,serverSelectionTimeoutMS=MONGO_TIEMPO_FUERA)
+
+    baseDatos=cliente[MONGO_BASEDATOS]
+    coleccion=baseDatos["maduradores"]
+
+    # Insertamos el documento
+    coleccion.update_one({"_id":objid},{'$set':nuevos_datos})
+
+    # Cierro la conexión a la base de Datos
+    cliente.close()
+
+
+@app.post("/delete_madurador_by_id/",tags=["madurador"])
+def delete_madurador_by_id(id):
+
+    objid=ObjectId(id)
+
+    #Defino el string de Conexion
+    cliente=pymongo.MongoClient(MONGO_URI,serverSelectionTimeoutMS=MONGO_TIEMPO_FUERA)
+
+    baseDatos=cliente[MONGO_BASEDATOS]
+    coleccion=baseDatos["maduradores"]
+
+    # Insertamos el documento
+    coleccion.delete_one({"_id":objid})
+
+    # Cierro la conexión a la base de Datos
+    cliente.close()
+
 @app.get("/get_all_madurador/",tags=["madurador"])
 def find_all_madurador():
 
@@ -60,17 +109,29 @@ def find_all_madurador():
     lista = []
 
     for documento in documentos:
-        lista.append(documento)
+            lista.append(documento)
 
     cliente.close()
 
-    return Response(lista).toDict()
+    return str(Response(lista).toDict())
 
 
-# @app.get("/get_madurador_by_id/{item_id}",tags=["madurador"])
-# def find_madurador_by_id():
+@app.get("/get_madurador_by_id/",tags=["madurador"])
+def find_madurador_by_id(id):
+
+    objid=ObjectId(id)
+     
+    #Defino el string de Conexion
+    cliente=pymongo.MongoClient(MONGO_URI,serverSelectionTimeoutMS=MONGO_TIEMPO_FUERA)
+
+    baseDatos=cliente[MONGO_BASEDATOS]
+    coleccion=baseDatos["maduradores"]
+
+    # Ejecuto un find para traer todos los documentos de la colección y los asigno a la variable documentos
+    documentos = coleccion.find_one({"_id":objid})
+
+    cliente.close()
+
+    return str(Response(documentos).toDict())
     
-
-# @app.post("/delete_madurador_by_id/{item_id}",tags=["madurador"])
-# def delete_madurador_by_id():
     
